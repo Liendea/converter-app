154094db59843481211fd5f60dbe37a2e485cbfc
Linda <bengtsson-linda@outlook.com>
2026-02-26 11:30:12 +0100

    removed comments

--- utils/CurrencyApi.ts
+++ utils/CurrencyApi.ts
@@ -1,6 +1,5 @@
 export const fetchExchangeRates = async (amount: number, from: string) => {
   try {
-    // Vi tar bort &to-parametern helt för att få alla valutor
     const response = await fetch(
       `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${from}`,
     );
@@ -11,7 +10,6 @@ export const fetchExchangeRates = async (amount: number, from: string) => {
 
     const data = await response.json();
 
-    // API:et returnerar ett objekt där 'rates' innehåller alla valutor
     return data.rates;
   } catch (error) {
     console.error("Error fetching exchange rates:", error);
